b513496cc5b519b5723ff036750aae2c9e05b4b5
Matheus1221 <tritanius1221@gmail.com>
2026-02-14 01:37:39 -0300

    create backend project

---
+++ assets-manager-backend/src/test/java/com/matheus/assetsmanager/AssetsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.assetsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
